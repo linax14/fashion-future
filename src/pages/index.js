92c8c626ef101c5852b8c089864c8102c7734c8a
linax14 <vbondarciuc@pm.me>
2025-05-08 12:53:46 +0100

    sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 document.querySelector('#logged-side-nav').remove()
 let sideNav = new CreateElement('nav').setAttributes({ class: 'side-nav', id: 'main-side-nav' }).appendTo(document.body)
-let main = new CreateElement('div').appendTo(document.body)
+let main = new CreateElement('div').setAttributes({ id: 'index-main' }).appendTo(document.body)
+let section = new CreateElement('section').setAttributes({ id: 'index-section' }).appendTo(main)
 
 let authContainer = new CreateElement('div').setAttributes({ class: 'auth-container' }).appendTo(main)
 let authFormContainer = new CreateElement('div').setAttributes({ class: 'auth-form-container' }).appendTo(authContainer)
@@ -12,6 +13,7 @@ let displayInHome = (type) => {
     let bottomNav = document.querySelector('.bottom-nav')
     switch (type) {
         case 'welcome':
+            setDisplay([section], 'flex')
             setDisplay([bottomNav], 'none')
             if (authContainer) setDisplay([authContainer], 'none')
             break;
@@ -19,6 +21,7 @@ let displayInHome = (type) => {
         case 'auth':
             closeSideNav()
             setDisplay([authContainer], 'flex')
+            setDisplay([section], 'none')
             authFormContainer.innerHTML = ''
             break
         default:
@@ -26,7 +29,68 @@ let displayInHome = (type) => {
     }
 }
 
-displayInHome('welcome')
+let contentSections = (title, list, id, appendTo) => {
+
+    let container = new CreateElement('div').setAttributes({ id: id }).appendTo(appendTo)
+    new CreateElement('h2').setText(title).appendTo(container)
+    let ol = new CreateElement('ol').appendTo(container)
+    list.forEach(li => { new CreateElement('li').setText(li).appendTo(ol) })
+
+    return container
+}
+
+mainRender()
+
+function mainRender() {
+    displayInHome('welcome')
+
+    new CreateElement('h2').setText('Fashion Future').appendTo(section)
+
+    new CreateElement('p').setText(`Discover a new way to manage your wardrobe. 
+        With Fashion Future you can create outfits, track your
+        wardrobe and make sustainable choices.`).setAttributes({ class: 'index-p' }).appendTo(section)
+
+    let cta = new CreateElement('div').setAttributes({ class: 'repeat' }).appendTo(section)
+    let ol = new CreateElement('ol').setAttributes({ class: 'repeat-li' }).appendTo(cta)
+    let list = ['Reduce', ' Reuse', ' Recycle', 'Rethink ', 'Refuse', ' Repair', 'Refashion it!']
+    list.forEach(li => new CreateElement('li').setText(li).appendTo(ol))
+
+    let clone = ol.cloneNode(true)
+    cta.appendChild(clone)
+
+    let s1 = contentSections('Why choose our virtual closet?', [
+        'Sustainability Quizzes',
+        'Challenges tailored to you',
+        `Outfit Creation`,
+        `Wardrobe and Wear stats`
+    ], 'why-us', section)
+
+    let imgs = new CreateElement('div').appendTo(s1)
+    new CreateElement('img').setAttributes({ src: '../assets/index/quiz.jpg', class: 'index-img', id: 'left' }).appendTo(imgs)
+    new CreateElement('img').setAttributes({ src: '../assets/index/stats.jpg', class: 'index-img', id: 'right' }).appendTo(imgs)
+
+    let s2 = contentSections('How it works', [
+        `Upload your clothes`,
+        `Build your wardrobe`,
+        `Complete challenges and quizzes`,
+        `Earn points and discover your personality`
+    ], 'how', section)
+
+    let imgs2 = new CreateElement('div').appendTo(s2)
+    new CreateElement('img').setAttributes({ src: '../assets/index/planner.jpg', class: 'index-img', id: 'right' }).appendTo(imgs2)
+    new CreateElement('img').setAttributes({ src: '../assets/index/dash.jpg', class: 'index-img', id: 'left' }).appendTo(imgs2)
+
+    let s3 = contentSections('Ready?', [
+        `Start building your sustainable wardrobe today!`,
+        `Sign up and unlock a more organised and eco-friendly experience.`
+    ], 'ready', section)
+
+    new CreateElement('button').setAttributes({ class: 'btn' }).setText('Sign me up!').addEventListener('click', () => {
+        renderSignUpForm()
+    }).appendTo(s3)
+
+    return section
+}
 
 let passwordVisibility = (appendTo) => {
 
@@ -64,45 +128,7 @@ let closeSideNav = () => {
 }
 
 signUp.addEventListener('click', () => {
-    displayInHome('auth')
-    new CreateElement('h2').setText('Join Fashion Future').appendTo(authFormContainer)
-
-    let signUpForm = new CreateElement('form').setAttributes({ id: 'sign-up' }).appendTo(authFormContainer)
-    let firstName = new CreateElement('input').setAttributes({ type: 'text', placeholder: 'First Name' }).appendTo(signUpForm)
-    let lastName = new CreateElement('input').setAttributes({ type: 'text', placeholder: 'Last Name' }).appendTo(signUpForm)
-    let birthday = new CreateElement('input').setAttributes({ type: 'date' }).appendTo(signUpForm)
-    let emailInput = new CreateElement('input').setAttributes({ type: 'email', placeholder: 'E-mail' }).appendTo(signUpForm)
-
-    let passwordInputDiv = new CreateElement('div').appendTo(signUpForm)
-    let passwordInput = new CreateElement('input').setAttributes({ type: 'password', placeholder: 'Password', minlength: 8, id: 'password' }).appendTo(passwordInputDiv)
-    passwordVisibility(passwordInputDiv)
-
-    let selectTheme = new CreateElement('div').appendTo(signUpForm)
-    new CreateElement('small').setText('Select an app theme. PS: You can change it later if you want!').appendTo(selectTheme)
-    let theme = new CreateElement('select').setAttributes({ name: 'theme' }).setText('theme').appendTo(selectTheme)
-
-    let options = ['dark', 'light']
-    options.forEach(option => {
-        new CreateElement('option').setAttributes({ value: option }).setText(`${option}`).appendTo(theme)
-    })
-
-    let formSubmit = new CreateElement('button').setAttributes({ class: 'btn' }).setText('sign up').appendTo(signUpForm)
-    let signIn = new CreateElement('p').setText(`Already have an account? `).appendTo(authFormContainer)
-    new CreateElement('span').setText('Sign in').setAttributes({ style: `border-bottom:1px solid white` }).addEventListener('click', () => { signInFormClickHandler() }).appendTo(signIn)
-
-    signUpForm.addEventListener('submit', async (event) => {
-        event.preventDefault()
-
-        await signUpUser(emailInput.value, passwordInput.value, firstName.value,
-            lastName.value, birthday.value, theme.value)
-
-        if (signUpUser) {
-            let userData = await signInWithEmail(emailInput.value, passwordInput.value)
-            if (userData) {
-                window.location.href = './public/dashboard.html'
-            } return
-        }
-    })
+    renderSignUpForm()
 })
 
 let signUpUser = async (userEmail, userPassword, firstName, lastName, birthday, theme) => {
@@ -142,10 +168,52 @@ let signInWithEmail = async (emailInput, passwordInput) => {
     }
 }
 
+function renderSignUpForm() {
+    displayInHome('auth')
+
+    new CreateElement('h2').setText('Join Fashion Future').appendTo(authFormContainer)
+
+    let signUpForm = new CreateElement('form').setAttributes({ id: 'sign-up' }).appendTo(authFormContainer)
+    let firstName = new CreateElement('input').setAttributes({ type: 'text', placeholder: 'First Name' }).appendTo(signUpForm)
+    let lastName = new CreateElement('input').setAttributes({ type: 'text', placeholder: 'Last Name' }).appendTo(signUpForm)
+    let birthday = new CreateElement('input').setAttributes({ type: 'date' }).appendTo(signUpForm)
+    let emailInput = new CreateElement('input').setAttributes({ type: 'email', placeholder: 'E-mail' }).appendTo(signUpForm)
+
+    let passwordInputDiv = new CreateElement('div').appendTo(signUpForm)
+    let passwordInput = new CreateElement('input').setAttributes({ type: 'password', placeholder: 'Password', minlength: 8, id: 'password' }).appendTo(passwordInputDiv)
+    passwordVisibility(passwordInputDiv)
+
+    let selectTheme = new CreateElement('div').appendTo(signUpForm)
+    new CreateElement('small').setText('Theme').appendTo(selectTheme)
+    let theme = new CreateElement('select').setAttributes({ name: 'theme', id: 'select-theme' }).setText('theme').appendTo(selectTheme)
+
+    let options = ['dark', 'earthy', 'light']
+    options.forEach(option => {
+        new CreateElement('option').setAttributes({ value: option }).setText(`${option}`).appendTo(theme)
+    })
+
+    let formSubmit = new CreateElement('button').setAttributes({ class: 'btn' }).setText('sign up').appendTo(signUpForm)
+    let signIn = new CreateElement('p').setText(`Already have an account? `).appendTo(authFormContainer)
+    new CreateElement('span').setText('Sign in').setAttributes({ style: `border-bottom:1px solid white` }).addEventListener('click', () => { signInFormClickHandler() }).appendTo(signIn)
+
+    signUpForm.addEventListener('submit', async (event) => {
+        event.preventDefault()
+
+        await signUpUser(emailInput.value, passwordInput.value, firstName.value,
+            lastName.value, birthday.value, theme.value)
+
+        if (signUpUser) {
+            let userData = await signInWithEmail(emailInput.value, passwordInput.value)
+            if (userData) {
+                window.location.href = './public/dashboard.html'
+            } return
+        }
+    })
+}
+
 function renderSideNav() {
     let header = new CreateElement('div').setAttributes({ class: 'header' }).appendTo(sideNav)
     let icon = new CreateElement('i').setAttributes({ class: 'fa-solid fa-bars' }).appendTo(header)
-    let name = new CreateElement('h2').setText('Fashion Future').appendTo(header)
     let ol = new CreateElement('ol').appendTo(sideNav)
 
     icon.addEventListener('click', () => {
@@ -157,6 +225,9 @@ function renderSideNav() {
         }
     })
 
+    new CreateElement('li').setAttributes({ class: 'btn' }).setText('About us').addEventListener('click', () => {
+        displayInHome('welcome')
+    }).appendTo(ol)
     let signIn = new CreateElement('li').setAttributes({ class: 'btn' }).setText('sign in').appendTo(ol)
     let signUp = new CreateElement('li').setAttributes({ class: 'btn' }).setText('sign up').appendTo(ol)
     return { signIn, icon, signUp }
